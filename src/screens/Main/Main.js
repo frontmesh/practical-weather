08d5cd21dc97b9160456bfa1074cd1560ca75549
Vladimir Vujosevic <v.vujosevic@meteovista.nl>
2018-07-05 16:32:27 +0200

    feat(add-redux): main screen wip

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,9 +1,34 @@
-/**
- * Main
- */
-class Main {
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+import { THEME } from '../../config';
+const { COLOR: { BLACK }, PRIMARY } = THEME;
+
+class Main extends Component {
+  static navigationOptions = ({
+    navigation: { navigate, state: { params: { backgroundColor = PRIMARY } = { } } },
+  }) => ({
+    headerStyle: {
+      backgroundColor,
+      elevation: 0,
+      shadowColor: 'transparent',
+      shadowRadius: 0,
+      shadowOffset: {
+        height: 0,
+      },
+    },
+  });
+
   constructor(props) {
     super(props);
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text>Hello</Text>
+      </View>
+    );
   }
 }
 
