23f2ddd3080d5d55de8867f9ce52610eeb0c84db
Vladimir Vujosevic <vladimirvuj@gmail.com>
2018-07-09 01:07:35 +0200

    feat(navigation): add main

--- src/screens/Main/Main.js
+++ src/screens/Main/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Permissions } from 'react-native';
+import { Constants, Location } from 'expo';
 
 import { THEME } from '../../config';
 const { COLOR: { BLACK }, PRIMARY } = THEME;
@@ -8,6 +9,8 @@ class Main extends Component {
   static navigationOptions = ({
     navigation: { navigate, state: { params: { backgroundColor = PRIMARY } = { } } },
   }) => ({
+    headerTransparent: true,
+    title: 'Blabla',
     headerStyle: {
       backgroundColor,
       elevation: 0,
@@ -21,6 +24,22 @@ class Main extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      location: null,
+      errorMessage: null,
+    };
+  }
+
+  getLocation = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
   }
 
   render() {
